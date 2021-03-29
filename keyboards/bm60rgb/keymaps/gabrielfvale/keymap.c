76cef4887376d5bacabecc99f4c737a74134beec
Gabriel Freire <gabrielfvale@gmail.com>
2021-03-29 16:17:30 -0300

    [bm60rgb] Move and create macros

--- keyboards/bm60rgb/keymaps/gabrielfvale/keymap.c
+++ keyboards/bm60rgb/keymaps/gabrielfvale/keymap.c
@@ -20,6 +20,8 @@ enum custom_keycodes {
     A_TILDE,
     O_TILDE,
     TERMINAL,
+    COPY,
+    PASTE,
 };
 
 enum tap_dance {
@@ -48,6 +50,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case TERMINAL:
         if (record->event.pressed) { SEND_STRING(SS_LCTL(SS_LALT("t"))); }
         break;
+    case COPY:
+        if (record->event.pressed) { SEND_STRING(SS_LCTL("c")); }
+        break;
+    case PASTE:
+        if (record->event.pressed) { SEND_STRING(SS_LCTL("v")); }
+        break;
     }
     return true;
 };
@@ -63,15 +71,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [1] = LAYOUT(
         RESET,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  RGB_TOG,
         _______, RGB_HUD, RGB_HUI, RGB_SAD, RGB_SAI, _______, _______, _______, _______, _______, _______, KC_MPRV, KC_MNXT, KC_MPLY,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_GRAVE, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,        RGB_VAI, KC_DEL,
         _______, _______, _______,                      _______,                         _______, _______, RGB_RMOD, RGB_VAD, RGB_MOD
     ),
     [2] = LAYOUT(
         KC_TILD,   KC_P1,   KC_P2,   KC_P3,   KC_P4,   KC_P5,   KC_P6,   KC_P7,   KC_P8,   KC_P9,   KC_P0,  KC_PDOT,  KC_PEQL,  _______,
         _______,   _______, _______, _______, _______, TERMINAL, _______, _______, _______, O_TILDE, _______, _______, _______, _______,
-        _______,   A_TILDE, _______, _______, _______, _______,  _______, _______, _______, _______, _______, _______, _______,
-        _______,   _______, _______, C_CEDILLA, _______, _______, _______, _______, _______, _______, _______,          _______, _______,
+        _______,   A_TILDE, _______, _______, _______, _______,  _______, _______, _______, _______, C_CEDILLA, _______, _______,
+        _______,   _______, _______,    COPY,   PASTE, _______, _______, _______, _______, _______, _______,          _______, _______,
         _______,   _______, _______,                      _______,                           _______, _______, _______, _______, _______
     ),
 
--- keyboards/bm60rgb/keymaps/gabrielfvale/readme.md
+++ keyboards/bm60rgb/keymaps/gabrielfvale/readme.md
@@ -1,24 +1,32 @@
 # gabrielfvale's keymap for bm60rgb
+
 ## Main changes
-Tap dance enabled for LShift -> Tap (and hold) for shift, tap twice for CapsLock
+Tap dance enabled for LShift -> Tap (and hold) for Shift, tap twice for CapsLock
+
 CapsLock changed to access layer 2
+" becomes GRAVE on Layer 1
 ## Macros
 All macros are on layer 2.
-A: ~a
-O: ~o
-C: ¸c
-T: CTRL + ALT + T (opens a terminal window)
+**A** ~a
+**O** ~o
+**;** ¸c
+**T** CTRL + ALT + T (opens a terminal window)
+**C** CTRL + C (copy)
+**V** CTRL + V (paste)
+
 ## Multimedia
-[: Previous
-]: Next
-\: Play/pause
+**\[** Previous
+**\]** Next
+**\\** Play/pause
+
 ## RGB
-RGB is controller on the first layer.
+
+RGB is controlled on the first layer.
 Backspace: RGB toggle.
-Q: Decrease HUE
-W: Increase HUE
-E: Decrease SAT
-R: Increase SAT
-Arrow Up: Increase brightness
-Arrow Down: Decrease brightness
-Side Arrows: Select RGB effect
+**Q** Decrease HUE
+**W** Increase HUE
+**E** Decrease SAT
+**R** Increase SAT
+**Arrow Up** Increase brightness
+**Arrow Down** Decrease brightness
+**Side Arrows** Select RGB effect
